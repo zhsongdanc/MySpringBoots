9c57752debc1b9c3144da82fec90b3de21a8dc82
demussong <demussong@tencent.com>
2023-12-18 15:10:15 +0800

    todo-spring循环依赖

---
+++ cycleDependency/src/main/java/com/szh/CycleApplication.java
@@ -0,0 +1,17 @@
+package com.szh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Author: demussong
+ * @Description:
+ * @Date: 2023/12/1 11:11
+ */
+@SpringBootApplication
+public class CycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CycleApplication.class, args);
+    }
+}
---
+++ cycleDependency/src/main/java/com/szh/service/ServiceB.java
@@ -0,0 +1,16 @@
+package com.szh.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ * @Author: demussong
+ * @Description:
+ * @Date: 2023/12/1 11:13
+ */
+@Service
+public class ServiceB {
+
+    @Autowired
+    private ServiceA serviceA;
+}
